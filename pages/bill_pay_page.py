7d3839df67d81366f117a7044a148c14b36215f2
Ascend-Krithi <gkiruthika1505@gmail.com>
2026-02-18 10:40:17 +0530

    Update bill_pay_page.py

--- pages/bill_pay_page.py
+++ pages/bill_pay_page.py
@@ -7,44 +7,44 @@ from locators.locators import ParabankLocators
 class BillPayPage:
     def __init__(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(driver, 10)
+        self.wait = WebDriverWait(driver, 20) # Increased timeout for cloud stability
 
     def login(self, username, password):
-        self.driver.find_element(*ParabankLocators.USERNAME_FIELD).send_keys(username)
+        self.wait.until(EC.visibility_of_element_located(ParabankLocators.USERNAME_FIELD)).send_keys(username)
         self.driver.find_element(*ParabankLocators.PASSWORD_FIELD).send_keys(password)
-        self.driver.find_element(*ParabankLocators.LOGIN_BUTTON).click()
+        # Ensure button is clickable before clicking
+        login_btn = self.wait.until(EC.element_to_be_clickable(ParabankLocators.LOGIN_BUTTON))
+        login_btn.click()
 
     def navigate_to_bill_pay(self):
-        for i in range(3):
+        for _ in range(3):
             try:
-                link = self.wait.until(EC.element_to_be_clickable(ParabankLocators.BILL_PAY_LINK))
-                link.click()
+                self.wait.until(EC.element_to_be_clickable(ParabankLocators.BILL_PAY_LINK)).click()
                 return True
             except StaleElementReferenceException:
                 time.sleep(1)
         return False
 
-    def enter_field_slowly(self, locator, text):
-        element = self.driver.find_element(*locator)
-        element.clear()
-        element.send_keys(text)
-        time.sleep(0.8) # Slows down each field entry
-
     def fill_bill_pay_form(self, data):
-        self.enter_field_slowly(ParabankLocators.PAYEE_NAME, data['payee'])
-        self.enter_field_slowly(ParabankLocators.ADDRESS, data['address'])
-        self.enter_field_slowly(ParabankLocators.CITY, data['city'])
-        self.enter_field_slowly(ParabankLocators.STATE, data['state'])
-        self.enter_field_slowly(ParabankLocators.ZIP_CODE, data['zip'])
-        self.enter_field_slowly(ParabankLocators.PHONE, data['phone'])
-        self.enter_field_slowly(ParabankLocators.ACCOUNT_NO, data['acc'])
-        self.enter_field_slowly(ParabankLocators.VERIFY_ACCOUNT_NO, data['acc'])
-        self.enter_field_slowly(ParabankLocators.AMOUNT_FIELD, "100")
+        # Using a list of tuples to maintain order and simplify code
+        fields = [
+            (ParabankLocators.PAYEE_NAME, data['payee']),
+            (ParabankLocators.ADDRESS, data['address']),
+            (ParabankLocators.CITY, data['city']),
+            (ParabankLocators.STATE, data['state']),
+            (ParabankLocators.ZIP_CODE, data['zip']),
+            (ParabankLocators.PHONE, data['phone']),
+            (ParabankLocators.ACCOUNT_NO, data['acc']),
+            (ParabankLocators.VERIFY_ACCOUNT_NO, data['acc']),
+            (ParabankLocators.AMOUNT, "100")
+        ]
+        for locator, value in fields:
+            element = self.wait.until(EC.visibility_of_element_located(locator))
+            element.clear()
+            element.send_keys(value)
+            time.sleep(0.3) 
         
-        btn = self.driver.find_element(*ParabankLocators.SEND_PAYMENT_BTN)
-        btn.click()
-        time.sleep(2) # Pause to see the click
+        self.driver.find_element(*ParabankLocators.SEND_PAYMENT_BTN).click()
 
     def get_confirmation_text(self):
-        element = self.wait.until(EC.visibility_of_element_located(ParabankLocators.CONFIRMATION_MSG))
-        return element.text
+        return self.wait.until(EC.visibility_of_element_located(ParabankLocators.CONFIRMATION_MSG)).text
