0ba6a8c6126efede51fc15ccde923e98ae51a361
Ascend-Krithi <gkiruthika1505@gmail.com>
2026-02-18 10:39:51 +0530

    Update locators.py

--- locators/locators.py
+++ locators/locators.py
@@ -1,15 +1,16 @@
 from selenium.webdriver.common.by import By
 
 class ParabankLocators:
-    # Login Elements
+    # Login
     USERNAME_FIELD = (By.NAME, "username")
     PASSWORD_FIELD = (By.NAME, "password")
     LOGIN_BUTTON = (By.CSS_SELECTOR, "input[value='Log In']")
+    LOGOUT_LINK = (By.XPATH, "//a[contains(@href, 'logout.htm')]") # or (By.XPATH, "//a[text()='Log Out']")
     
     # Navigation
-    BILL_PAY_LINK = (By.XPATH, "//a[text()='Bill Pay']")
+    BILL_PAY_LINK = (By.LINK_TEXT, "Bill Pay")
     
-    # Bill Pay Form Elements
+    # Bill Pay Form
     PAYEE_NAME = (By.NAME, "payee.name")
     ADDRESS = (By.NAME, "payee.address.street")
     CITY = (By.NAME, "payee.address.city")
@@ -18,8 +19,6 @@ class ParabankLocators:
     PHONE = (By.NAME, "payee.phoneNumber")
     ACCOUNT_NO = (By.NAME, "payee.accountNumber")
     VERIFY_ACCOUNT_NO = (By.NAME, "verifyAccount")
-    AMOUNT_FIELD = (By.NAME, "amount")
+    AMOUNT = (By.NAME, "amount")
     SEND_PAYMENT_BTN = (By.CSS_SELECTOR, "input[value='Send Payment']")
-    
-    # Assertion Elements
     CONFIRMATION_MSG = (By.ID, "billpayResult")
