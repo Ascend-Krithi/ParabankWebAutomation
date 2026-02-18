33c4ef33299e25efc95147b3b59969ed01e24050
Ascend-Krithi <gkiruthika1505@gmail.com>
2026-02-18 11:37:32 +0530

    Update test_bill_pay.py

--- tests/test_bill_pay.py
+++ tests/test_bill_pay.py
@@ -38,7 +38,7 @@ class TestParabankBillPay(unittest.TestCase):
         self.bp_page = BillPayPage(self.driver)
 
     def test_bill_payment_flow(self):
-        self.bp_page.login("aavademo", "Ascendion_1")
+        self.bp_page.login("AAVA", "ascendion@1")
         
         wait = WebDriverWait(self.driver, 20)
         wait.until(EC.url_contains("overview.htm"))
